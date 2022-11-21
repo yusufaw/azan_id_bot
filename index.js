4cd4106ca86b2dc29a4251eb83f154ed0de9a9c3
Yusuf Aji Wibowo <yusufaw@proton.me>
2022-11-22 00:59:05 +0800

    Add some function to generate message

--- index.js
+++ index.js
@@ -13,30 +13,20 @@ const currentLocation = {
 
 const currentTimezone = find(currentLocation.latitude, currentLocation.longitude)
 const currentFormattedDate = moment().tz(currentTimezone[0]).format("yyyy-MM-DD");
-const currentFormattedDateX = moment().locale("id").tz(currentTimezone[0]).format("dddd, DD MMMM yyyy");
-console.log(currentFormattedDateX)
+const currentFormattedDateWithDay = moment().locale("id").tz(currentTimezone[0]).format("dddd, DD MMMM yyyy");
 
 bot.command('jadwal', ctx => {
     console.log(ctx.message.text);
 
     axios.get(`https://waktu-sholat.vercel.app/prayer?latitude=${currentLocation.latitude}&longitude=${currentLocation.longitude}`)
         .then(function (response) {
-            const currentDay = response.data.prayers.filter(age => {
+            const currentListTime = response.data.prayers.filter(age => {
                 return age.date === currentFormattedDate
-            });
-            console.log(currentDay);
-            var message = `<b>[Waktu Salat Hari Ini]</b>
-${currentFormattedDateX}
-<code>`
+            })[0].time;
 
-            for (var key in currentDay[0].time) {
-                if (currentDay[0].time.hasOwnProperty(key)) {
-                    message = message + `
-${toSalahSchedule(key, currentDay[0].time[key])}`;
-                }
-            }
-            message = message + "</code>"
-            ctx.reply(message, {
+            const formattedMessage = generateFormattedMessage(currentFormattedDateWithDay, currentListTime)
+
+            ctx.reply(formattedMessage, {
                 parse_mode: "HTML",
                 disable_web_page_preview: true,
             })
@@ -55,7 +45,21 @@ String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
 };
 
-function toSalahSchedule(key, value) {
+function generateSalahSchedule(key, value) {
     const spaceCount = 8 - key.length
     return key.toProperCase() + new Array(spaceCount + 1).join(' ') + "- " + value
+}
+
+function generateFormattedMessage(day, time) {
+    var message = "<b>[Waktu Salat Hari Ini]</b>"
+    message += `\n<i>${day}</i>\n`
+    message += "<code>"
+
+    for (var key in time) {
+        if (time.hasOwnProperty(key)) {
+            message += `\n${generateSalahSchedule(key, time[key])}`;
+        }
+    }
+    message += "</code>"
+    return message;
 }
