81303bb07a5d810dda6e41641f28951c982b83b1
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2022-12-18 01:53:05 +0700

    Add $DIGI_O_USERNAME

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ssh -tt fi0smith@$DIGI_IP << ENDSSH
-   cd /home/fi0smith/workspace/azan_id_bot/
+ssh -tt $DIGI_O_USERNAME@$DIGI_O_IP << ENDSSH
+   cd /home/$DIGI_O_USERNAME/workspace/azan_id_bot/
    git pull origin main
    npm install
-   pm2 restart all
+   pm2 restart azan_id_bot
    exit
 ENDSSH
