b3815887508be06fc72127092889c87d5026ceae
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2022-12-18 01:34:21 +0700

    Try use ENDSSH

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ssh -tt fi0smith@$DIGI_IP<<EOF
+ssh -tt fi0smith@$DIGI_IP << ENDSSH
    cd /home/fi0smith/workspace/azan_id_bot/
    git pull origin main
    npm install
    pm2 restart all
-EOF
+ENDSSH
