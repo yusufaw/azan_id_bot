8f175de349fe2435951666b535ed69cb16d0bbc5
Yusuf Aji Wibowo <yusufaw@users.noreply.github.com>
2023-11-04 21:41:19 +0700

    Add timestamps to location schema (#8)

--- model/LocationsModel.js
+++ model/LocationsModel.js
@@ -1,9 +1,15 @@
+const { Schema } = require('mongoose');
 const mongoose = require('../lib/db');
 
-module.exports = mongoose.model('location', {
-  chat_id: String,
-  chat_name: String,
-  latitude: Number,
-  longitude: Number,
-  city: String
-});
+
+const locationSchema = new Schema(
+  {
+    chat_id: String,
+    chat_name: String,
+    latitude: Number,
+    longitude: Number,
+    city: String
+  }, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+})
+module.exports = mongoose.model('location', locationSchema);
